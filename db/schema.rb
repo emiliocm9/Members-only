827c4cc517e52d8cc0751cb2c7514b8d6b2acad1
emiliocm9 <emilio.contreras97@gmail.com>
2020-07-29 16:14:33 -0500

    FIX linter corrections

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2020_07_29_192218) do
-
   create_table 'posts', force: :cascade do |t|
     t.string 'author'
     t.string 'title'
@@ -23,7 +22,7 @@ ActiveRecord::Schema.define(version: 2020_07_29_192218) do
 
   create_table 'users', force: :cascade do |t|
     t.string 'email', default: '', null: false
-    t.string 'encrypted_password', default: "", null: false
+    t.string 'encrypted_password', default: '', null: false
     t.string 'reset_password_token'
     t.datetime 'reset_password_sent_at'
     t.datetime 'remember_created_at'
